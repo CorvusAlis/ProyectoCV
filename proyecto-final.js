65f008384ee51f0d727411a67e85e25932e107cf
CorvusAlis <barbara.tobares@gmail.com>
2023-02-28 20:39:47 -0300

    agregado overlay oscuro al pop up

--- proyecto-final.js
+++ proyecto-final.js
@@ -1,13 +1,15 @@
 //al inicializar la pagina, el pop up no se ve
 document.getElementById('popup').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
 
 //al clickear el boton "Ver mas" se muestra el pop up
 document.getElementById('verMas').addEventListener('click', function(){
     document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
 });
 
 //al clickear el boton "X" se cierra el pop up
 document.getElementById('cerrar').addEventListener('click', function(){
-    document.body.style.opacity = '1.0';
     document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
 });
